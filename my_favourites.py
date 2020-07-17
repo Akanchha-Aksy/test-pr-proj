8ba23edf8f3c39d482718f52f8c28c3d3cb7b921
Akanchha Gupta <akanchha.agupta@nuance.com>
2020-07-17 08:34:50 -0400

    Added fruit like

--- my_favourites.py
+++ my_favourites.py
@@ -1,3 +1,3 @@
 
 print("I like books - {book1}, {book2}, {book3}".format(book1="Rich Dad, Poor Dad", book2="Factfulness", book3="Just Friends"))
-print("I love mangoes, coconut")
+print("I love mangoes, coconut, lichi")
