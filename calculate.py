2607254e077caaf081e94247313246400f72ce7a
Akanchha Gupta <akanchha.agupta@nuance.com>
2020-07-17 06:38:03 -0400

    added two files

---
+++ calculate.py
@@ -0,0 +1,14 @@
+
+
+class Calculator:
+
+    def plus(num1, num2):
+        return num1 + num2
+
+    def minus(num1, num2):
+        return num1 - num2
+
+
+#print(Calculator.plus(2, 3))
+obj = Calculator
+print(obj.plus(3,7))
