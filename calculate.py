38c61fe7e0ad58884c00c312c50725fee6749d71
Akanchha Gupta <akanchha.agupta@nuance.com>
2020-07-22 05:43:31 -0400

    updated Calculate.py

--- calculate.py
+++ calculate.py
@@ -1,14 +1,11 @@
 
-
 class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
 
-    def plus(num1, num2):
-        return num1 + num2
+    def plus(self):
+        return self.num1 + self.num2
 
-    def minus(num1, num2):
-        return num1 - num2
-
-
-#print(Calculator.plus(2, 3))
-obj = Calculator
-print(obj.plus(3,7))
+    def minus(self):
+        return self.num1 - self.num2
