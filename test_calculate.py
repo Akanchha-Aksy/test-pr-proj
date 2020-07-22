7a6ebf87e201f636271db82d852c4316e688864e
Akanchha Gupta <akanchha.agupta@nuance.com>
2020-07-22 05:16:10 -0400

    added changes

--- test_calculate.py
+++ test_calculate.py
@@ -1,5 +1,10 @@
+import six
 
-from calculate import Calculator
+import hello_world
 
-def test_plus():
-    assert Calculator.plus(4,5) == 9
+# def test_plus():
+#     six.add_move("ss")
+#     obj = Calculator
+#     assert obj.plus(obj,2,3) ==5
+def ge():
+    hello_world
