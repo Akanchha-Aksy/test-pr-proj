5e197394bb12a14e947b76851da48c223f397cd7
Akanchha Gupta <akanchha.agupta@nuance.com>
2020-07-22 08:28:18 -0400

    Working on subtree

--- test_calculate.py
+++ test_calculate.py
@@ -1,10 +1,9 @@
-import six
-
+from calculate import Calculator
 import hello_world
 
-# def test_plus():
-#     six.add_move("ss")
-#     obj = Calculator
-#     assert obj.plus(obj,2,3) ==5
-def ge():
+def test_plus():
+    obj = Calculator(2, 3)
+    assert obj.plus() == 5
+
+def test_hello_world():
     hello_world
