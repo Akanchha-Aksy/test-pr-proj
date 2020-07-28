f82fde988a66cc044e24f9335ed2dfe02d9e8fe9
Akanchha Gupta <akanchha.agupta@nuance.com>
2020-07-28 05:14:41 -0400

    removing subtree

--- test_module/my_favourites.py
+++
@@ -1,4 +0,0 @@
-
-print("I like books - {book1}, {book2}, {book3}".format(book1="Rich Dad, Poor Dad", book2="Factfulness", book3="Just Friends"))
-print("I love mangoes, coconut, banana and lichi")
-
--- test_module/test_me.py
+++
@@ -1,5 +0,0 @@
-import hello_world
-import my_favourites
-print("This is for testing packaging")
-hello_world
-my_favourites
